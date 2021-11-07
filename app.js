0cbf96eba8f0996e6c588ede3ab382fb87257e21
Rebecca Nayala <rebeccanayala@gmail.com>
2021-11-07 08:57:37 -0300

    Making the sliding animation

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const inputs = document.querySelectorAll(".input-field");
+const toggle_btn = document.querySelectorAll(".toggle");
+const main = document.querySelector("main");
 
 inputs.forEach(inp => {
     inp.addEventListener("focus", () => {
@@ -7,5 +9,11 @@ inputs.forEach(inp => {
     inp.addEventListener("blur", () => {
         if(inp.value != "") return;
         inp.classList.remove("active");
+    });
+});
+
+toggle_btn.forEach(btn => {
+    btn.addEventListener("click", () => {
+        main.classList.toggle("sign-up-mode");
     });
 });
