5e023eb4837b1047f9971b0b5b9cfecac5fa9b6e
Rebecca Nayala <rebeccanayala@gmail.com>
2021-11-07 23:01:32 -0300

    Creating the carousel

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const inputs = document.querySelectorAll(".input-field");
 const toggle_btn = document.querySelectorAll(".toggle");
 const main = document.querySelector("main");
+const bullets = document.querySelectorAll(".bullets span");
+const images = document.querySelectorAll(".image");
 
 inputs.forEach(inp => {
     inp.addEventListener("focus", () => {
@@ -17,3 +19,14 @@ toggle_btn.forEach(btn => {
         main.classList.toggle("sign-up-mode");
     });
 });
+
+function moveSlider() {
+    let index = this.dataset.value;
+    console.log(index);
+    bullets.forEach(bull => bull.classList.remove("active"));
+    this.classList.add("active");
+}
+
+bullets.forEach(bullet => {
+    bullet.addEventListener("click", moveSlider);
+})
